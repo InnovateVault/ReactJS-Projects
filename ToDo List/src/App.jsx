329655b3e8db4f0ad0dff9da54aaf575d8b6f60b
InnovateVault <Y88071932@gmail.com>
2024-08-18 02:15:38 +0100

    App Complete

--- ToDo List/src/App.jsx
+++ ToDo List/src/App.jsx
@@ -1,9 +1,55 @@
-import "./Styles.css"
+import { NewToDoForm } from "./components/NewToDoForm"
+import { ToDoList } from "./components/ToDoList"
+import { useState, useEffect } from "react"
+import "./styles.css"
 
 
 
 const App = () => {
-    return "Hello"
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+
+  function addToDo(title) {
+    setTodos((currentToDos) => {
+      return [
+        ...currentToDos, { id: crypto.randomUUID(), title, completed: false }
+      ]
+    })
   }
-  
-  export default App
+
+  function toggleTodo(id, completed) {
+    setTodos(currentToDos => {
+      return currentToDos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentToDos => {
+      return currentToDos.filter(todo => todo.id !== id)
+    })
+  }
+
+  return (
+    <>
+      <NewToDoForm onSubmit={addToDo} />
+      <h1 className="header">ToDo List</h1>
+      <ToDoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </>
+  )
+}
+
+export default App
